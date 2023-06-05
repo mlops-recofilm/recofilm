59e916aaf3111048165b122dc8d1654db847a5b5
Isaac Cohen Sabban <isaac.cohensabban@jellysmack.com>
2023-06-05 19:35:40 +0200

    Add checking data function

---
+++ preprocessing/check_data.py
@@ -0,0 +1,25 @@
+import numpy as np
+import glob
+
+csv_path = glob.glob('../ml-20m/*csv')
+
+
+def get_information_data(path:str):
+    df = pd.read_csv(path)
+    print(path)
+    print("****** Stat desc ******")
+    print(df.describe())
+    print("****** Correlation ******")
+    numeric_columns = df.select_dtypes(include=[np.number]).columns
+    correlation_matrix = df[numeric_columns].corr()
+    print(correlation_matrix)
+    print("****** NA ******")
+    print((df.isna().sum()*100)/df.shape[0])
+    print("****** Null ******")
+    print((df.isnull().sum()*100)/df.shape[0])
+    print("****** Valeurs uniques ******")
+    print(df.nunique())
+
+
+for path in csv_path:
+    get_information_data(path)
