9dbcc2899cd93174e1d1e27197e285151b4a719a
Isaac Cohen Sabban <isaac.cohensabban@jellysmack.com>
2023-06-13 16:33:49 +0200

    Fix issue on movie model

--- model/movie_model.py
+++ model/movie_model.py
@@ -2,33 +2,33 @@ import pandas as pd
 import numpy as np
 from scipy.sparse import csr_matrix
 from sklearn.neighbors import NearestNeighbors
+from joblib import dump, load
 
 
 class MovieModel:
-    def __init__(self, df:pd.DataFrame):
-        self.data = df[['movieId','rating', 'userId']]
-        self.model = None
-        self.rating_matrix = None
 
-    def prepare_data(self):
-        rating_matrix = self.data.pivot(index='movieId', columns='userId', values='rating').fillna(0)
+    @staticmethod
+    def prepare_data(df: pd.DataFrame):
+        df = df[['movieId', 'rating', 'userId']].drop_duplicates()
+        rating_matrix = df.pivot(index='movieId', columns='userId', values='rating').fillna(0)
         return csr_matrix(rating_matrix.values)
 
-    def fit(self, n_neighbors: int):
-        self.rating_matrix = self.prepare_data()
-        self.model = NearestNeighbors(metric='cosine', algorithm='brute', n_neighbors=n_neighbors)
-        self.model.fit(self.rating_matrix)
+    def fit(self, df: pd.DataFrame, n_neighbors: int):
+        rating_matrix = self.prepare_data(df)
+        model = NearestNeighbors(metric='cosine', algorithm='brute', n_neighbors=n_neighbors)
+        model.fit(rating_matrix)
+        dump(model, 'movie_model.joblib')
 
-    def predict(self, df, movie_title: str, num_recommendations: int):
-        movie_id = df[df['title'] == movie_title]['movieId'].unique()
-        distances, indices = self.model.kneighbors(self.rating_matrix[movie_id], n_neighbors=num_recommendations)
+    def predict(self, df: pd.DataFrame, movie_title: str, num_recommendations: int, title_dict: dict):
+        model = load('movie_model.joblib')
+        movie_id = title_dict[movie_title]
+        rating_matrix = self.prepare_data(df)
+        distances, indices = model.kneighbors(rating_matrix[movie_id], n_neighbors=num_recommendations)
 
         # drop the first index being the movie itself
-        indices = np.delete(indices, 0)
+        #indices = np.delete(indices, 0)
 
         # print titles of those movies
-        recommendations = []
-        for i in indices:
-            recommendations.append(df['title'][i])
+        recommendations = df[['movieId', 'rating', 'userId','title']].drop_duplicates().reset_index(drop=True)['title'][indices[0]].to_list()
         return recommendations
 
