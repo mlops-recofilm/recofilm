878a2d2638b90d05e07c31f8f08f257d33ec61e4
Isaac Cohen Sabban <isaac.cohensabban@jellysmack.com>
2023-06-13 16:33:21 +0200

    Update training pipeline

--- training_pipeline.py
+++ training_pipeline.py
@@ -1,5 +1,12 @@
 from preprocessing.create_data import Data
 from model.movie_model import MovieModel
+from model.utils import split_data
 
-df = Data()
-movie_model = MovieModel(df = df).fit(20)
+TEST_SIZE = 0.3
+
+df = Data(min_relevance=0.3).data
+train, test, title_dict = split_data(df)
+MovieModel().fit(train, 10)
+title = 'Star Wars: Episode IV - A New Hope (1977)'
+recommendations = MovieModel().predict(test, title, 10, title_dict)
+print(recommendations)
