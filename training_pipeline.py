1835eda1a426663fb6274617e2b0576ba04ce28c
isaaccs <isaaccohensabban@gmail.com>
2023-06-27 16:31:14 +0200

    Update training pipeline

--- training_pipeline.py
+++ training_pipeline.py
@@ -1,12 +1,15 @@
 from preprocessing.create_data import Data
 from model.movie_model import MovieModel
-from model.utils import split_data
+from model.utils import split_data_random, check_overlap
 
 TEST_SIZE = 0.3
 
 df = Data(min_relevance=0.3).data
-train, test, title_dict = split_data(df)
-MovieModel().fit(train, 10)
+train, test, title_dict = split_data_random(df)
+check_overlap(train, test)
+MovieModel().fit(df, 10)
 title = 'Star Wars: Episode IV - A New Hope (1977)'
-recommendations = MovieModel().predict(test, title, 10, title_dict)
+recommendations = MovieModel().evaluate(df, title, 10, title_dict)
+MovieModel().stability(df, title, 10, title_dict)
+MovieModel().prediction_comparaison(df, ['Star Wars: Episode IV - A New Hope (1977)', 'Junior (1994)', 'Interview with the Vampire: The Vampire Chronicles (1994)'], 10, title_dict)
 print(recommendations)
