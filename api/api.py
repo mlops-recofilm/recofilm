08a96de3788efa393888ecc77fcf92a3f439d31a
Isaac Cohen Sabban <isaac.cohensabban@jellysmack.com>
2023-08-09 07:17:36 +0200

    Add doctring and fix files path

--- api/api.py
+++ api/api.py
@@ -5,7 +5,7 @@ import os
 import random
 from joblib import load
 
-from utils.path import model_folder
+from utils.path import model_folder, output_folder
 from api_utils.utils import *
 
 
@@ -13,6 +13,12 @@ data, movie_data, user_data, title_dict = get_data()
 
 
 def get_next_new_userid():
+    """
+    Get the next available new user ID.
+
+    Returns:
+        int: The next available new user ID.
+    """
     NEXT_NEW_USERID = os.getenv('NEXT_NEW_USERID')
     if not NEXT_NEW_USERID: # variable not defined
         NEXT_NEW_USERID = data.userId.max() + 1
@@ -53,24 +59,56 @@ def read_root():
 
 @app.get("/unique_genres", tags=['utils'])
 def unique_genres():
+    """
+    Get the list of unique movie genres.
+
+    Returns:
+        dict: A dictionary containing a key 'genres' with a list of unique movie genres.
+    """
     all_genres = data['genres'].str.split('|', expand=True).stack().tolist()
     unique_genres = sorted(set(all_genres))
     return {"genres": unique_genres}
 
+
 @app.get("/unique_movies", tags=['utils'])
 def unique_movies():
+    """
+    Get the list of unique movie titles.
+
+    Returns:
+        dict: A dictionary containing a key 'movies' with a list of unique movie titles.
+    """
     all_movies = data['title'].unique().tolist()
     return {"movies": all_movies}
 
+
 @app.get("/random", tags=['model'])
 def random_output(query_params: dict = Depends(query_params)):
+    """
+    Generate a random movie recommendation for a user.
+
+    Args:
+        query_params (dict): The query parameters containing 'user_id' and 'subject'.
+
+    Returns:
+        dict: A dictionary containing a key 'movie' with the recommended movie title.
+    """
     unseen_movies = get_unseen_movies(data, query_params['user_id'], query_params['subject'])
     random_movie = random.choice(unseen_movies)
     return {"movie": [k for k, v in title_dict.items() if v == random_movie][0]}
 
 
 @app.get("/movie_model", tags=['model'])
-def random_output(query_params: dict = Depends(query_params)):
+def movie_model(query_params: dict = Depends(query_params)):
+    """
+    Generate a random movie recommendation using the movie recommendation model.
+
+    Args:
+        query_params (dict): The query parameters containing 'user_id' and 'movie_name'.
+
+    Returns:
+        dict: A dictionary containing a key 'movie' with the recommended movie title.
+    """
     model = load(os.path.join(model_folder, 'movie_model.joblib'))
     movie_id = title_dict[query_params['movie_name']]
     distances, indices = model.kneighbors(movie_data[movie_id], n_neighbors=10)
@@ -81,9 +119,18 @@ def random_output(query_params: dict = Depends(query_params)):
 
 @app.get("/remindMe/{k}", tags=['historical'])
 def remind_reco(k: int, userid: Annotated[str, Depends(get_user_credentials)]) -> List[str]:
-    """Remind last k unique recommended movies"""
+    """
+    Get the last k unique recommended movies for a user.
 
-    with open ("../docker_volume/outputs/predictions_history.json", "r") as f:
+    Args:
+        k (int): The number of unique recommended movies to retrieve.
+        userid (str): The user ID.
+
+    Returns:
+        List[str]: A list of last unique recommended movie titles.
+    """
+
+    with open(os.path.join(output_folder,"predictions_history.json"), "r") as f:
         recommended_movies = json.loads(f.read())
 
     list_movies = recommended_movies[userid]['movies']
@@ -97,12 +144,33 @@ def remind_reco(k: int, userid: Annotated[str, Depends(get_user_credentials)]) -
 
 @app.post("/addRating", tags=['add data'])
 def add_rating(movieid: str, rating: float, userid: Annotated[str, Depends(get_user_credentials)]) -> bool:
+    """
+    Add a movie rating for a user.
+
+    Args:
+        movieid (str): The movie ID.
+        rating (float): The movie rating.
+        userid (str): The user ID.
+
+    Returns:
+        bool: True if the rating was added successfully, False otherwise.
+    """
     success = add_ratings(userid, movieid, rating)
     return success
 
 
 @app.get("/bestMoviesByGenre", tags=['add data'])
 def get_best_movies_by_genre(genre: GenreEnum, userid: Annotated[str, Depends(get_user_credentials)]) -> List[str]:
+    """
+    Get the best movies of a specific genre for a user.
+
+    Args:
+        genre (GenreEnum): The movie genre.
+        userid (str): The user ID.
+
+    Returns:
+        List[str]: A list of best movie titles of the specified genre.
+    """
     top_movies_by_genre = data[data['genres'].str.contains(genre.value)]\
                                .sort_values('rating', ascending=False)\
                                 .head(10)[['movieId','title']]
@@ -114,9 +182,15 @@ def get_best_movies_by_genre(genre: GenreEnum, userid: Annotated[str, Depends(ge
 
 @app.post("/createUser", tags=['add data'])
 def create_user(new_ratings: RatingsItem) -> int:
-    """Creates a new user by adding
-    Returns freshly created user id."""
-    # make sure to have the latest version of data
+    """
+    Create a new user and add movie ratings for the user.
+
+    Args:
+        new_ratings (RatingsItem): The new movie ratings provided for the user.
+
+    Returns:
+        int: The freshly created user ID.
+    """
     data = get_data()
 
     # new_userId = int(data.userId.max()) + 1
@@ -138,7 +212,7 @@ def create_user(new_ratings: RatingsItem) -> int:
             detail=f"Movie with id {movieid} doesn't exist, please enter an existing movie",
         )
 
-    with open("../docker_volume/next_new_userid", "r+") as next_new_userid_file:
+    with open(os.path.join(output_folder,"next_new_userid"), "r+") as next_new_userid_file:
         next_new_userid = next_new_userid_file.read()
         next_new_userid = int(next_new_userid)
         if next_new_userid == -1:
