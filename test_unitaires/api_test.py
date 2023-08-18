bcd0abd807d4b979a84e5728d984ab5db80fa8a7
hf2503 <hfmole@hotmail.com>
2023-08-18 15:21:51 +0200

    test_4

--- test_unitaires/api_test.py
+++ test_unitaires/api_test.py
@@ -24,54 +24,8 @@ print(encoded_credentials)
 
 
 def test_api_starting():
-    """check if the API is running."""
+    """Test if the API is running."""
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "API is up and running"}
 
-def test_api_unique_genre():
-    """check if the list of unique movies genres is not empty"""
-    response = client.get('/unique_genres')
-    assert response.status_code == 200
-    assert response.json() != None
-
-def test_api_get_random():
-    """check if the api gives always a random movie with an user known"""
-    response = client.get('/random?user_id=1644')
-    assert response.status_code == 200
-    assert response.json() != {'message': 'no movie for you:('}
-
-def test_api_user_model():
-    """ check if the api_user_model gives always a movie advised by the model movie with an user and a movie known:
-    user_id : 1644"""
-    response = client.get('/usermodel',params={'user_id':'1644'},
-                          headers={"Authorization": "Basic MTY0NDo="})
-    assert response.status_code == 200
-    assert response.json() != {'message': 'no movie for you:('}
-
-def test_api_movie_model():
-    """check if the api_movie_model gives always a movie advised by the model movie with an user and a movie known:
-    user_id : 1644
-    movie : Star Wars: Episode IV - A New Hope (1977) """
-    
-    response = client.get('/movie_model',params={'user_id':'1644',
-                          'movie_name':'Star Wars: Episode IV - A New Hope (1977)'},
-                          headers={"Authorization": "Basic MTY0NDo="})
-    assert response.status_code == 200
-    assert response.json() != {'message': 'no movie for you:('}
-
-
-def test_api_user_model():
-    """ check if the api_user_model gives always a movie advised by the model movie with an user and a movie known:
-    user_id : 1644"""
-    response = client.get('/user_model',params={'user_id':'1644'},
-                          headers={"Authorization": "Basic MTY0NDo="})
-    assert response.status_code == 200
-    assert response.json() != {'message': 'no movie for you:('}
-
-def  test_api_reminder_2():
-   """ check if api_reminder gives 10 movies with k = 10 (parameter) and userId = 1644 """
-   response = client.get('/remindMe',params={'k':'10'},
-                          headers={"Authorization": "Basic MTY0NDo="})
-   assert response.status_code == 200
-   assert len(response.json().get('movie')) == 10
