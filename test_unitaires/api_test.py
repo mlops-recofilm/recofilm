76e1a79f5b3b193a672af725de5a0e619bbf947a
isaaccs <isaaccohensabban@gmail.com>
2023-08-20 13:38:25 +0200

    mock get data

--- test_unitaires/api_test.py
+++ test_unitaires/api_test.py
@@ -18,11 +18,33 @@ from fastapi import FastAPI, HTTPException, Response, status, Depends, Header, Q
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from requests.auth import HTTPBasicAuth
 import time
+from unittest.mock import Mock
 
 mock_data = pd.DataFrame([[1,3.5,1644,
                            'Adventure|Animation|Children|Comedy|Fantasy','Toy Story (1995)']],
                            index=['1'],
                            columns=['movieId','rating','userId','genres','title'])
+
+
+rating_matrix = mock_data.pivot(index='movieId', columns='userId', values='rating').fillna(0)
+mock_movie_data = csr_matrix(rating_matrix.values)
+mock_user_data = mock_data.pivot(index='userId', columns='movieId', values='rating').fillna(0)
+mock_title_dict = pd.Series(data.movieId.values, index=data.title).to_dict()
+
+mock_get_data = Mock()
+
+
+mock_get_data.return_value = (
+    mock_data,
+    mock_movie_data,
+    mock_user_data,
+    mock_title_dict
+)
+
+
+result = mock_get_data()
+
+
 
 def test_unique_genres():
     with patch('api.api.data', mock_data):
